6fcb8236e2280365f73faef90b5968c2f5104d85
Hayo21 <user.email>
2026-08-08 14:07:24 +0700

    install tailwind and Make a Navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <nav class="px-2 sm:px-1 py-1  text-white bg-black text-xl font-bold">
+        <div class="flex items-center justify-around  p-3 ms-3">
+            <h1 class="">MUNIF</h1>
+            <div class="flex gap-5 ms-5 ">
+                <a href="" class="hover:text-red-300">HOME</a>
+                <a href="" class="hover:text-red-300">ABOUT</a>
+                <a href="" class="hover:text-red-300">CONTACT</a>
+            </div>
+        </div>
+    </nav>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <x-navbar />
+    <main>
+        <div class="flex justify-center items-center h-screen ">
+            <h1 class="text-4xl font-bold">Welcome to My Portfolio</h1>
+        </div>
+    </main>
+</body>
+
+</html>
